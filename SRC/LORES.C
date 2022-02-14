2fd2ab5c25dc261223aadf237ebe0f8c58bbd877
David Schmenk <dschmenk@sbcglobal.net>
2022-02-13 17:20:50 -0800

    Solidify EGA mode set

--- SRC/LORES.C
+++ SRC/LORES.C
@@ -13,7 +13,11 @@
 /*
  * CGA CRTC Registers
  */
-char gr160regs[] = {113, 80, 89, 15, 127, 6, 100, 112, 2, 1, 32, 0, 0, 0};
+unsigned char cga160crtc[] = {113, 80, 89, 15, 127, 6, 100, 112, 2, 1, 32, 0, 0, 0};
+unsigned char ega160misc   =  0x23;
+unsigned char ega160seq[]  = {0x03, 0x01, 0x03, 0x00, 0x03};
+unsigned char ega160crtc[] = {0x70, 0x4F, 0x5C, 0x2F, 0x5F, 0x07, 0x04, 0x11, 0x00, 0x01, 0x06, 0x07,
+             /* Start Addr */ 0x00, 0x00, 0x00, 0x00, 0xE1, 0x24, 0xC7, 0x28, 0x08, 0xE0, 0xF0, 0xA3, 0xFF};
 unsigned int scanline[100]; // Precalculated scanline offsets
 unsigned char borderColor;
 unsigned int scrnMask;
@@ -37,29 +41,54 @@ void txt80(void)
  */
 int gr160(unsigned char fill, unsigned char border)
 {
-    int i, wfill, charrows, adapter;
+    int i, wfill, chrows, adapter;
     int far *wvidmem = (int far *)0xB8000000L;
+    unsigned char far *switches = (unsigned char far *)0x00000410L;
+    unsigned char save_switches;
     union REGS regs;
 
-    /* Start off in text mode 3 */
+    /* Start off in text mode 3, 200 scanlines */
+
     regs.x.ax = 3;
     int86(0x10, &regs, &regs);
-    /* Get character generator information */
-    regs.x.ax = 0x1130;
+    regs.x.ax = 0x1130; /* Get character generator information */
     regs.x.bx = 0x0000;
     regs.x.cx = 0x0000;
     int86(0x10, &regs, &regs);
-    charrows = regs.h.cl;
-    if (charrows)
+    chrows = regs.h.cl;
+    if (chrows)
     {
-        regs.x.ax = 0x1110; /* Load user character set */
-        regs.x.bx = (charrows / 4) << 8;
-        regs.x.cx = 1;
-        regs.x.dx = 0;
-        int86(0x10, &regs, &regs); /* Set quearter height characters */
-        regs.x.ax = 0x1003;
-        regs.x.bx = 0x0000;
-        int86(0x10, &regs, &regs); /* turn off blink via EGA/VGA BIOS */
+        if (chrows != 16)
+        { /* EGA */
+            outp(0x3C4, 0); outp(0x3C5, 0x00); /* Sequencer reset */
+            outp(0x3C2, ega160misc); /* reprogram into 200 scanline mode */
+            for (i = 1; i < sizeof(ega160seq); i++)
+            {
+                outp(0x3C4, i); outp(0x3C5, ega160seq[i]);
+            }
+            for (i = 0; i < sizeof(ega160crtc); i++)
+            {
+                outp(0x3D4, i); outp(0x3D5, ega160crtc[i]);
+            }
+    		inp(0x3DA); /* Read the input status register to reset the VGA attribute controller index/data state */
+    		outp(0x3c0, 0x10); /* ttribute controller index register, mode register select */
+    		i  = inp(0x3C1); /* VGA attribute controller data register, read mode register */
+    		i &= 0xF7; /* turn off bit 3, blink */
+    		outp(0x3C0, 0x10); /* select the attribute control register */
+    		outp(0x3C0, i);	/* write to VGA attribute controller data register */
+            outp(0x3C4, 0); outp(0x3C5, 0x03); /* Sequencer reset */
+        }
+        else
+        { /* VGA */
+            regs.x.ax = 0x1110; /* Load user character set */
+            regs.x.bx = (chrows / 4) << 8;
+            regs.x.cx = 1;
+            regs.x.dx = 0;
+            int86(0x10, &regs, &regs); /* Set quarter height characters */
+            regs.x.ax = 0x1003;
+            regs.x.bx = 0x0000;
+            int86(0x10, &regs, &regs); /* turn off blink via BIOS */
+        }
         scrnMask = 0x7FFF;
         adapter = NOT_CGA;
     }
@@ -67,9 +96,9 @@ int gr160(unsigned char fill, unsigned char border)
     { /* CGA */
         /* Set CRTC registers */
         outp(0x3D8, 0x00); /* Turn off video */
-        for (i = 0; i < 14; i++)
+        for (i = 0; i < sizeof(cga160crtc); i++)
         {
-            outp(0x3D4, i); outp(0x3D5, gr160regs[i]);
+            outp(0x3D4, i); outp(0x3D5, cga160crtc[i]);
         }
         outp(0x3D8, 0x09);      // Turn off blink attribute
         borderColor = border & 0x0F;
