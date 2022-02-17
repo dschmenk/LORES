82df94fd9ba767de79875b5fdcd5529ab53caae7
David Schmenk <dschmenk@sbcglobal.net>
2022-02-17 13:18:32 -0800

    MapEdit internal format set

--- SRC/MAPEDIT/MAPEDIT.C
+++ SRC/MAPEDIT/MAPEDIT.C
@@ -2,9 +2,24 @@
 #include <stdlib.h>
 #include <dos.h>
 #include <conio.h>
-
+#define ESCAPE          0x001B
+#define LEFT_ARROW      0x4B00
+#define RIGHT_ARROW     0x4D00
+#define UP_ARROW        0x4800
+#define DOWN_ARROW      0x5000
+#define SCREEN_WIDTH    40
+#define SCREEN_HEIGHT   24
+#define STATUS_LINE     SCREEN_HEIGHT
+#define CENTER_X        (SCREEN_WIDTH/2)
+#define CENTER_Y        (SCREEN_HEIGHT/2)
+#define TILE_WIDTH      16
+#define TILE_HEIGHT     16
+#define MAP_WIDTH       16
+#define MAP_HEIGHT      16
+int mapWidth  = MAP_WIDTH;
+int mapHeight = MAP_HEIGHT;
 unsigned char far *vidmem = (char far *)0xB8000000L;
-unsigned char *tilemap[16][16];
+unsigned char *tileMap[MAP_HEIGHT * MAP_WIDTH];
 unsigned char grass[] = {  0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22,
                            0x22, 0x2A, 0x22, 0x22, 0x22, 0x22, 0x32, 0x22,
                            0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22,
@@ -92,38 +107,65 @@ unsigned char *map[] = {"................",
                         "#......*......*.",
                         "##.*....*.##.*..",
                         ".###.....****..."};
+unsigned char *expandTile(unsigned char *tileptr)
+{
+    int row, col;
+    unsigned char *expTile;
+    expTile = malloc(16*16);
+    for (row = 0; row < TILE_HEIGHT; row++)
+    {
+        for (col = 0; col < TILE_WIDTH; col += 2)
+        {
+            expTile[row * 16 + col]     = tileptr[row * 8 + col / 2] & 0x0F;
+            expTile[row * 16 + col + 1] = tileptr[row * 8 + col / 2] >> 4;
+        }
+    }
+    return expTile;
+}
 void buildmap(void)
 {
     int row, col;
-    unsigned char *tileptr;
+    unsigned char *tileptr, *grassExp, *treeExp, *dirtExp, *waterExp;
 
-    for (row = 0; row < 16; row++)
+    grassExp = expandTile(grass);
+    treeExp  = expandTile(tree);
+    dirtExp  = expandTile(dirt);
+    waterExp = expandTile(water);
+    for (row = 0; row < mapHeight; row++)
     {
-        for (col = 0; col < 16; col++)
+        for (col = 0; col < mapWidth; col++)
         {
             switch (map[row][col])
             {
                 case '.':
-                    tileptr = grass;
+                    tileptr = grassExp;
                     break;
                 case '*':
-                    tileptr = tree;
+                    tileptr = treeExp;
                     break;
                 case '#':
-                    tileptr = dirt;
+                    tileptr = dirtExp;
                     break;
                 case '~':
-                    tileptr = water;
+                    tileptr = waterExp;
                     break;
                 default:
                     tileptr = NULL;
                     break;
             }
-            tilemap[row][col] = tileptr;
+            tileMap[row * mapWidth + col] = tileptr;
         }
     }
 }
+unsigned short extgetch(void)
+{
+    unsigned short extch;
 
+    extch = getch();
+    if (!extch)
+        extch = getch() << 8;
+    return extch;
+}
 void txt40(void)
 {
     union REGS regs;
@@ -158,21 +200,26 @@ int tile(int x, int y, int s, int t, int width, int height)
     int pixaddr;
 
     pixaddr = y * 80 + (x << 1);
-    if ((x < 20) && ((x + width)  >= 20)
-     && (y < 14) && ((y + height) >= 14))
+    if ((x <= CENTER_X) && ((x + width)  > CENTER_X)
+     && (y <= CENTER_Y) && ((y + height) > CENTER_Y))
         borderColor = 0x0F;
     else
         borderColor = 0x0;
-    if (s >= 0 && t >= 0 && (s >> 4) < 16 && (t >> 4) < 16)
-        tile = tilemap[t >> 4][s >> 4];
+    if (s >= 0 && t >= 0 && (s >> 4) < mapWidth && (t >> 4) < mapHeight)
+    {
+        tile = tileMap[(t >> 4) * mapWidth + (s >> 4)];
+        tileChar = 0xB1;
+    }
     else
+    {
         tile = NULL;
+        tileChar = 0xDB;
+    }
     if (tile)
     {
         s &= 0x0F;
         t &= 0x0F;
-        tile   += t * 8 + (s >> 1);
-        width >>= 1;
+        tile   += t * 16 + s;
         while (height--)
         {
             ss = s;
@@ -189,13 +236,7 @@ int tile(int x, int y, int s, int t, int width, int height)
                     else if (t == 15)
                         tileChar = 0xC0;
                 }
-                vidmem[pixaddr + (w << 2)]     = tileChar;
-                vidmem[pixaddr + (w << 2) + 1] = (tile[w] & 0xF0) | borderColor;
-                ss++;
-                tileChar = 0x20; // space
-                if (t == 0 || t == 15)
-                    tileChar = 0xC4;
-                if (ss == 15)
+                else if (ss == 15)
                 {
                     tileChar = 0xB3;
                     if (t == 0)
@@ -203,13 +244,13 @@ int tile(int x, int y, int s, int t, int width, int height)
                     else if (t == 15)
                         tileChar = 0xD9;
                 }
-                vidmem[pixaddr + (w << 2) + 2] = tileChar;
-                vidmem[pixaddr + (w << 2) + 3] = (tile[w] << 4) | borderColor;
+                vidmem[pixaddr + (w << 1)]     = tileChar;
+                vidmem[pixaddr + (w << 1) + 1] = (tile[w] << 4) | borderColor;
                 ss++;
             }
             t++;
             pixaddr = pixaddr + 80;
-            tile   += 8;
+            tile   += 16;
         }
     }
     else
@@ -219,7 +260,7 @@ int tile(int x, int y, int s, int t, int width, int height)
         {
             for (w = 0; w < width; w++)
             {
-                vidmem[pixaddr + (w << 1)]     = 0xB1;
+                vidmem[pixaddr + (w << 1)]     = tileChar;
                 vidmem[pixaddr + (w << 1) + 1] = borderColor;
             }
             pixaddr = pixaddr + 80;
@@ -231,64 +272,80 @@ void tileRow(int y, int s, int t, int height)
 {
     int x;
 
-    x = 16 - (s & 0x0F);
+    x = TILE_WIDTH - (s & (TILE_WIDTH-1));
     tile(0, y, s, t, x, height);
-    s = (s + 16) & 0xFFF0;
-    for (; 40 - x > 16; x += 16)
+    s = (s + TILE_WIDTH) & ~(TILE_WIDTH-1);
+    for (; SCREEN_WIDTH - x > TILE_WIDTH; x += TILE_WIDTH)
     {
-        tile(x, y, s, t, 16, height);
-        s += 16;
+        tile(x, y, s, t, TILE_WIDTH, height);
+        s += TILE_WIDTH;
     }
-    tile(x, y, s, t, 40 - x, height);
+    tile(x, y, s, t, SCREEN_WIDTH - x, height);
 }
 void tileScrn(int s, int t)
 {
     int y;
 
-    y = 16 - (t & 0x0F);
+    y = TILE_HEIGHT - (t & (TILE_HEIGHT-1));
     tileRow(0, s, t, y);
-    t = (t + 16) & 0xFFF0;
-    for (; 25 - y > 16; y += 16)
+    t = (t + TILE_HEIGHT) & ~(TILE_HEIGHT-1);
+    for (; SCREEN_HEIGHT - y > TILE_HEIGHT; y += TILE_HEIGHT)
     {
-        tileRow(y, s, t, 16);
-        t += 16;
+        tileRow(y, s, t, TILE_HEIGHT);
+        t += TILE_HEIGHT;
     }
-    tileRow(y, s, t, 25 - y);
+    tileRow(y, s, t, SCREEN_HEIGHT - y);
 }
 /*
  * Tile map editor
  */
 int main(int argc, char **argv)
 {
-    int orgS, orgT;
-    char quit;
+    int orgS, orgT, extS, extT, centerS, centerT;
+    unsigned char *centerTile, tilePix;
+    char quit, cycle;
 
     orgS = 0;
     orgT = 0;
+    extS = mapWidth  << 4;
+    extT = mapHeight << 4;
     quit = 0;
     buildmap();
     txt40();
     do
     {
         tileScrn(orgS, orgT);
-        switch(getch())
+        centerS = orgS + CENTER_X;
+        centerT = orgT + CENTER_Y;
+        centerTile = tileMap[(centerT >> 4) * mapWidth + (centerS >> 4)];
+        if (centerTile)
+            tilePix = centerTile[(centerT & 0x0F) * 16 + (centerS & 0x0F)] << 4;
+        else
+            tilePix = 0x00;
+        vidmem[CENTER_Y * SCREEN_WIDTH * 2 + CENTER_X * 2] = 0xCE;
+        while (!kbhit())
         {
-            case 'i':
-            case 'I':
-                orgT--;
+            vidmem[CENTER_Y * SCREEN_WIDTH * 2 + CENTER_X * 2 + 1] = tilePix | (cycle++ & 0x0F);
+        }
+        switch(extgetch())
+        {
+            case UP_ARROW:
+                if (centerT > 0)
+                    orgT--;
                 break;
-            case 'm':
-            case 'M':
-                orgT++;
+            case DOWN_ARROW:
+                if (centerT < extT - 1)
+                    orgT++;
                 break;
-            case 'j':
-            case 'J':
-                orgS -= 2;
+            case LEFT_ARROW:
+                if (centerS > 0)
+                    orgS--;
                 break;
-            case 'k':
-            case 'K':
-                orgS += 2;
+            case RIGHT_ARROW:
+                if (centerS < extS - 1)
+                    orgS++;
                 break;
+            case ESCAPE:
             case 'q':
             case 'Q':
                 quit = 1;
