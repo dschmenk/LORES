c20b5c6bb9638ffd5d2ecaf8454bc315eeee0b4b
David Schmenk <dschmenk@sbcglobal.net>
2022-05-19 17:07:44 -0700

    Invaders from Outer Space test and sprite boundary fix for library

--- SRC/INVADERS/INVADERS.C
+++ SRC/INVADERS/INVADERS.C
@@ -8,17 +8,50 @@
 #include "tiler.h"
 #include "mapio.h"
 #include "keyboard.h"
-#define SCREEN_WIDTH        160
-#define SCREEN_HEIGHT       100
-#define ORG_X               16
-#define ORG_Y               96
-#define SHIP_Y              (ORG_Y+SCREEN_HEIGHT-shipHeight)
-#define MAX_INVADERS        5
-#define SHIP_SPRITE         0
-#define MISSILE_SPRITE      1
-#define INVADER_SPRITE_BASE 2
-#define INVADER_ALIVE       0
-#define INVADER_INC         5
+/*
+ * PPI stands for Programmable Peripheral Interface (which is the Intel 8255A chip)
+ * The PPI ports are only for IBM PC and XT, however port A is mapped to the same
+ * I/O address as the Keyboard Controller's (Intel 8042 chip) output buffer for compatibility.
+ */
+#define PPI_PORT_A              0x60
+#define PPI_PORT_B              0x61
+#define PPI_PORT_C              0x62
+#define PPI_COMMAND_REGISTER    0x63
+/*
+ * 8253 Programmable Interval Timer ports and registers
+ * The clock input is a 1.19318 MHz signal
+ */
+#define CLOCK_FREQ              1193180L
+#define PIT_COMMAND_REGISTER    0x43
+#define PIT_COUNT_REGISTER      0x42
+
+#define SET_SOUND(cnt)          do{outp(PIT_COMMAND_REGISTER, 0xB6);  \
+                                 outp(PIT_COUNT_REGISTER, (cnt));   \
+                                 outp(PIT_COUNT_REGISTER, ((cnt)>>8));}while(0)
+#define START_SOUND             do{outp(PPI_PORT_B, inp(PPI_PORT_B)|0x03);}while(0)
+#define STOP_SOUND               outp(PPI_PORT_B, inp(PPI_PORT_B)&0xFC)
+/*
+ * Screen dimensions
+ */
+#define SCREEN_WIDTH            160
+#define SCREEN_HEIGHT           100
+#define ORG_X                   16
+#define ORG_Y                   96
+/*
+ * Game defines
+ */
+#define SHIP_Y                  (ORG_Y+SCREEN_HEIGHT-shipHeight)
+#define SHIP_SPRITE             0
+#define MISSILE_SPRITE          1
+#define MISSILE_SPEED           4
+#define PEWPEW_COUNT            8
+#define MAX_INVADERS            6
+#define INVADER_INC             (MAX_INVADERS/2)
+#define INVADER_SPRITE_BASE     2
+#define INVADER_ALIVE           0
+#define QUIT_SUCCESS            -2
+#define QUIT_BAIL               -1
+#define QUIT_FAIL               (invaderSeq+1)
 /*
  * Tile map
  */
@@ -36,12 +69,31 @@ int shipWidth, shipHeight;
 int missileWidth, missileHeight, sizeofMissile;
 int invaderInc[MAX_INVADERS], invaderX[MAX_INVADERS], invaderY[MAX_INVADERS], invaderState[MAX_INVADERS];
 int invaderMove, invaderCount, invaderSeq, invaderWidth, invaderHeight, sizeofInvader;
+/*
+ * Sound sequences
+ */
+int alienBuzz[] =
+{
+    0x8000,
+    0x4000
+};
+int missileWhiz[] =
+{
+    0x2C00,
+    0x2A00,
+    0x2800,
+    0x2400,
+    0x2200,
+    0x2100,
+    0x2000,
+    0x1000,
+};
 
 int main(int argc, char **argv)
 {
     unsigned long st;
-    int quit, oldX, shipX, i;
-    int missileInFlight, missileX, missileY;
+    int quit, oldX, shipX, i, buzzRate, buzzIndex;
+    int missileInFlight, missileX, missileY, pewpew;
 
     if (!spriteLoad("ship.spr", &ship, &shipWidth, &shipHeight))
     {
@@ -84,7 +136,7 @@ int main(int argc, char **argv)
         invaderState[invaderCount] = INVADER_ALIVE;
         invaderInc[invaderCount]   = INVADER_INC;
         invaderX[invaderCount]     = ORG_X + SCREEN_WIDTH/MAX_INVADERS * invaderCount + SCREEN_WIDTH/MAX_INVADERS/4;
-        invaderY[invaderCount]     = ORG_Y + 1;
+        invaderY[invaderCount]     = ORG_Y + 1 + invaderCount * (invaderHeight + 1);
         spriteEnable(invaderCount + INVADER_SPRITE_BASE, invaderX[invaderCount], invaderY[invaderCount], invaderWidth, invaderHeight, invader);
     }
     /*
@@ -92,41 +144,16 @@ int main(int argc, char **argv)
      */
     for (i = 0; i < ORG_Y; i++)
         viewRefresh(SCROLL_UP);
+    pewpew    = 0;
+    buzzRate  = invaderCount << 2;
+    buzzIndex = 1;
+    SET_SOUND(alienBuzz[buzzIndex]);
+    START_SOUND;
     do
     {
 #ifdef PROFILE
         rasterBorder(WHITE);
 #endif
-        /*
-         * Update missile position
-         */
-        if (missileInFlight)
-        {
-            missileY -= 4;
-            /*
-             * Check missile hit on alien
-             */
-            for (i = 0; i < MAX_INVADERS; i++)
-                if (invaderState[i] == INVADER_ALIVE
-                && (missileX >= invaderX[i])
-                && (missileX + missileWidth <= invaderX[i] + invaderWidth)
-                && (missileY >= invaderY[i])
-                && (missileY + missileHeight <= invaderY[i] + invaderHeight))
-                {
-                    invaderState[i]++;
-                    missileY = 0;
-                }
-            if (missileY < ORG_Y-16)
-            {
-                /*
-                 * Missile off-map
-                 */
-                missileInFlight = 0;
-                spriteDisable(MISSILE_SPRITE);
-            }
-            else
-                spritePosition(MISSILE_SPRITE, missileX, missileY);
-        }
         /*
          * Update aliens
          */
@@ -139,14 +166,20 @@ int main(int argc, char **argv)
                 invaderX[invaderMove]   = (invaderX[invaderMove] < ORG_X) ? ORG_X : ORG_X + SCREEN_WIDTH - invaderWidth;
                 invaderY[invaderMove]  += invaderHeight + 1;
             }
-            spritePosition(invaderMove + INVADER_SPRITE_BASE, invaderX[invaderMove], invaderY[invaderMove]);
             if ((invaderY[invaderMove] + invaderHeight >= SHIP_Y)
              && (invaderX[invaderMove] + invaderWidth >= shipX)
              && (invaderX[invaderMove] <= shipX + shipWidth))
-             {
-                quit = MAX_INVADERS;
-                invaderMove++;
+            {
+                /*
+                 * Alien hit ship
+                 */
+                invaderState[invaderMove]++;
+                spriteDisable(SHIP_SPRITE);
+                invaderX[invaderMove] = shipX;
+                invaderY[invaderMove] = SHIP_Y;
+                quit                  = QUIT_FAIL;
             }
+            spritePosition(invaderMove + INVADER_SPRITE_BASE, invaderX[invaderMove], invaderY[invaderMove]);
         }
         else
         {
@@ -156,15 +189,72 @@ int main(int argc, char **argv)
             {
                 invaderState[invaderMove]++;
                 spriteDisable(invaderMove + INVADER_SPRITE_BASE);
-                quit = (--invaderCount == 0) ? -1 : 0;
+                quit = (--invaderCount == 0) ? QUIT_SUCCESS : 0;
             }
         }
-        if (!invaderMove--)
-            invaderMove = MAX_INVADERS - 1;
+        if (!quit)
+            do
+            {
+                /*
+                 * Find next active alien
+                 */
+                invaderMove = !invaderMove ? invaderMove = MAX_INVADERS - 1 : invaderMove - 1;
+            } while (invaderState[invaderMove] > invaderSeq);
+        if (missileInFlight > 0)
+        {
+            /*
+             * Update missile position
+             */
+            missileY -= MISSILE_SPEED;
+            if (missileY < ORG_Y-16)
+            {
+                /*
+                 * Missile off-map
+                 */
+                missileInFlight = 0;
+                spriteDisable(MISSILE_SPRITE);
+            }
+            else
+            {
+                spritePosition(MISSILE_SPRITE, missileX, missileY);
+                /*
+                 * Check missile hit on alien
+                 */
+                for (i = 0; i < MAX_INVADERS; i++)
+                    if ((invaderState[i] == INVADER_ALIVE)
+                     && (missileX >= invaderX[i] - INVADER_INC/2)
+                     && (missileX + missileWidth <= invaderX[i] + invaderWidth + INVADER_INC/2)
+                     && (missileY >= invaderY[i])
+                     && (missileY + missileHeight <= invaderY[i] + invaderHeight))
+                    {
+                        invaderState[i]++;
+                        missileInFlight = (ORG_Y-16 - missileY) / MISSILE_SPEED;
+                        spriteDisable(MISSILE_SPRITE);
+                    }
+            }
+        }
+        else if (missileInFlight < 0)
+            /*
+             * Missile reload time
+             */
+            missileInFlight++;
+        else if (keyboardGetKey(SCAN_SPACE))
+        {
+            /*
+             * Check for missile fired
+             */
+            pewpew          = PEWPEW_COUNT;
+            missileX        = shipX + 2;
+            missileY        = SHIP_Y - missileHeight;
+            missileInFlight = spriteEnable(MISSILE_SPRITE, missileX, missileY, missileWidth, missileHeight, missile);
+        }
+        /*
+         * Ship movement input
+         */
         oldX = shipX;
-        if (keyboardGetKey(SCAN_KP_4) || keyboardGetKey(SCAN_LEFT_ARROW)) // Turn left
+        if (keyboardGetKey(SCAN_KP_4) || keyboardGetKey(SCAN_LEFT_ARROW)) // Move left
             shipX--;
-        if (keyboardGetKey(SCAN_KP_6) || keyboardGetKey(SCAN_RIGHT_ARROW)) // Turn right
+        if (keyboardGetKey(SCAN_KP_6) || keyboardGetKey(SCAN_RIGHT_ARROW)) // Move right
             shipX++;
         if (oldX != shipX)
         {
@@ -173,38 +263,75 @@ int main(int argc, char **argv)
             else
                 spritePosition(SHIP_SPRITE, shipX, SHIP_Y);
         }
-        if (!missileInFlight && keyboardGetKey(SCAN_SPACE)) // Fire missile
+        if (!quit && keyboardGetKey(SCAN_ESC)) // Quit
+            quit = QUIT_BAIL;
+        /*
+         * Update sounds
+         */
+        if (pewpew)
         {
-            missileX        = shipX + 2;
-            missileY        = SHIP_Y - missileHeight;
-            missileInFlight = spriteEnable(MISSILE_SPRITE, missileX, missileY, missileWidth, missileHeight, missile);
+            if (--pewpew)
+                SET_SOUND(missileWhiz[pewpew]);
+            else
+            {
+                if (buzzRate == 0)
+                    buzzIndex ^= 1;
+                SET_SOUND(alienBuzz[buzzIndex]);
+            }
         }
-        if (keyboardGetKey(SCAN_ESC)) // Quit, but don't allow cheating if about to be hit by SAM
-            quit = -1;
+        else
+        {
+            if (buzzRate == 0)
+            {
+                buzzIndex ^= 1;
+                SET_SOUND(alienBuzz[buzzIndex]);
+            }
+        }
+        buzzRate = buzzRate == 0 ? invaderCount << 2: buzzRate - 1;
         viewRefresh(0);
     } while (!quit);
     /*
-     * Scroll out of view
+     * Clean up
      */
+    STOP_SOUND;
     if (missileInFlight)
         spriteDisable(MISSILE_SPRITE);
-    if (quit > 0)
-        spriteDisable(SHIP_SPRITE);
-    while (quit > 0)
+    /*
+     * Success/Fail banner
+     */
+    if (quit == QUIT_SUCCESS)
+        text(32, 0, DARKGREEN, "Earth Saved!");
+    else
     {
-        if (invaderState[invaderMove] < invaderSeq)
-            spriteUpdate(invaderMove + INVADER_SPRITE_BASE, invader + invaderState[invaderMove]++ * sizeofInvader);
-        else if (invaderState[invaderMove] == invaderSeq)
+        while (quit > 0)
         {
-            invaderState[invaderMove]++;
-            spriteDisable(invaderMove + INVADER_SPRITE_BASE);
+            /*
+             * Ship destroyed by alien so animate final explosion
+             */
+            if (invaderState[invaderMove] < invaderSeq)
+                spriteUpdate(invaderMove + INVADER_SPRITE_BASE, invader + invaderState[invaderMove]++ * sizeofInvader);
+            else if (invaderState[invaderMove] == invaderSeq)
+            {
+                invaderState[invaderMove]++;
+                spriteDisable(invaderMove + INVADER_SPRITE_BASE);
+            }
+            frameCount = 0;
+            while (frameCount < MAX_INVADERS)
+                statusRasterTimer();
+            viewRefresh(0);
+            quit--;
         }
-        frameCount = 0;
-        while (frameCount < MAX_INVADERS)
-            statusRasterTimer();
-        viewRefresh(SCROLL_DOWN);
-        quit--;
+        text(24, 0, DARKRED, "Earth Invaded!");
     }
+    /*
+     * Hold screen for 2 seconds
+     */
+    frameCount = 0;
+    while (frameCount < 120)
+        statusRasterTimer();
+    /*
+     * Scroll out of view
+     */
     for (i = 0; i < ORG_Y; i++)
         viewRefresh(SCROLL_DOWN);
     keyboardUninstallDriver();
---
+++ SRC/INVADERS/readme.md
@@ -0,0 +1,32 @@
+# Invaders from Outer Space
+
+https://youtu.be/1TRwAGdS_Xs
+
+Earth is being invaded by space aliens. You are Earth's last defense. Based on
+the Moon, you must shoot the aliens before they get down to you and destroy
+your ship. Otherwise, Earth is lost.
+
+                Controls:
+                    LEFT ARROW  - move left
+                    RIGHT ARROW - move right
+                    SPACEBAR    - fire missile
+                    ESCAPE      - quit game
+
+## Implementation Details
+
+Unlike the other testbed programs, "Invaders" only uses scrolling during the
+opening and closing scenes. Actual gameplay relies just on sprites to render
+the play field. There are a possible 8 active sprites: 1 ship, 1 missile, and 6
+aliens. A stock IBM PC cannot update all 8 sprites 60 times a second without
+CGA snow or image anomalies so alien movement is interleaved, one alien per
+frame. It is hard to recognize that the aliens aren't moving in sync.
+
+The background was created from a NASA "Earthrise" picture. The slicer.c
+program in the MAPEDIT directory is run on a modern computer and will convert
+the Portable Net RGB image into either a dithered or a closest match 4 BPP IRGB
+version. It will then slice the image in 16x16 pixel tiles, reducing duplicate
+tiles, and saving it in a format to be loaded by the LORES library (or further
+edited with MAPEDIT).
+
+These are more of the tricks needed to effectively use this library and
+graphics mode on a 4.77 MHz IBM PC.
